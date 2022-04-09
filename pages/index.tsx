6d2c3c58598f223eec266228ecab80fcb4fb1add
Genki Sato <genki.sato@lasca.co.jp>
2022-04-10 07:42:13 +0900

    add web3.js prototype

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+import { getItems } from "../lib/itemRequest"
+
 function Home() {
+  useEffect(() => {
+    (async() => {
+      const tokenMetadatas = await getItems()
+      console.log(tokenMetadatas)
+
+      const openseaUrl = `https://opensea.io/assets/${tokenMetadatas[0].contractAddress}/${tokenMetadatas[0].tokenId}`
+      console.log(openseaUrl)
+    })()
+  });
+
   return (
     <div>Welcome to Next.js!</div>
   )
